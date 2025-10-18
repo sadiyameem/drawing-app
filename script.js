27d374a53782a1263c6a74529623ee1c8e5deac4
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-18 19:33:41 -0400

    added more variables and now adding listeners

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 const canvas = document.getElementById("canvas");
 canvas.height = window.innerHeight -100;
 canvas.width = window.innerWidth -50;
+
+const context = canvas.getCounted("2d");
+
+let x = null, 
+    y = null;
+    context.lineWidth = 5;
+
+    let draw = false;
+    context.strokeStyle = "#010600";
